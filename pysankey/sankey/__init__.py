5b057839b5a2db1643358f30ae511dad29cd75e5
pintergreg <pintergreg@gmail.com>
2023-11-03 17:26:08 +0100

    Fix alphaDict types

--- pysankey/sankey/__init__.py
+++ pysankey/sankey/__init__.py
@@ -74,7 +74,7 @@ def sankey(
     figSize: Optional[Tuple[int, int]] = None,
     ax: Optional[Any] = None,
     color_gradient: bool = False,
-    alphaDict: dict = None,
+    alphaDict: Optional[dict[str | tuple[str, str], float]] = None
 ) -> Any:
     """
     Make Sankey Diagram showing flow from left-->right
@@ -382,8 +382,8 @@ def plot_strips(
     rightLabels: ndarray,
     rightWidths: Dict,
     xMax: float64,
+    alphaDict: dict[str|tuple[str, str], float],
     color_gradient: bool = False,
-    alphaDict: dict = None,
 ) -> None:
     # Plot strips
     for leftLabel in leftLabels:
